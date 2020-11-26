3bca7a1175a5d5891e633498053ff5168da412e7
Alex Lopes Pereira <alexlopespereira@gmail.com>
2020-11-26 20:22:57 -0300

    update

--- src/scrapy_crunchbase.py
+++ src/scrapy_crunchbase.py
@@ -19,7 +19,9 @@ class crawler():
         fp.set_preference("browser.download.manager.showWhenStarting", False)
         fp.set_preference("browser.download.dir", self.download_dir)
         fp.set_preference("browser.helperApps.neverAsk.saveToDisk",
-                          "text/plain, application/octet-stream, application/binary, text/csv, application/csv, application/excel, text/comma-separated-values, text/xml, application/xml")
+                          """text/plain, application/octet-stream, application/binary,
+                           text/csv, application/csv, application/excel, 
+                           text/comma-separated-values, text/xml, application/xml""")
         fp.set_preference("pdfjs.disabled", True)
         self.driver = webdriver.Firefox(firefox_profile=fp)
 
@@ -79,7 +81,9 @@ class crawler():
             button_search = self.driver.find_element_by_xpath('//button[@aria-label="Search"]')
             button_search.click()
             wait_element(self.driver, '//div[@class="cb-overflow-ellipsis"]', by=By.XPATH, to_sleep=2)
-            nresults = int(self.driver.find_element_by_xpath("//results-info[@class='flex-none hide show-gt-xs']/h3").text.split(" ")[-2].replace(",", ""))
+            nresults = int(self.driver.find_element_by_xpath(
+                            "//results-info[@class='flex-none hide show-gt-xs']/h3")\
+                           .text.split(" ")[-2].replace(",", ""))
             if nresults > threshold:
                 print(f"{sa.strftime('%Y/%m/%d')}, {ea.strftime('%Y/%m/%d')}. {nresults} > {threshold}")
             elif nresults > 0:
@@ -90,8 +94,8 @@ class crawler():
                 filename = f"{self.download_dir}/{self.search_name}-{today_str}.csv"
                 while not os.path.exists(filename):
                     time.sleep(1)
-
-                dest_file = f"{self.dest_dir}/{self.search_name}_{sa.strftime('%Y%m%d')}_{ea.strftime('%Y%m%d')}.csv"
+                dest_file = f"""{self.dest_dir}/{self.search_name}_
+                                {sa.strftime('%Y%m%d')}_{ea.strftime('%Y%m%d')}.csv"""
                 shutil.move(filename, dest_file)
 
     def crawl(self, url, type="funding"):
